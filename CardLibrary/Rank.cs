47e365fda1540828e3bdd8132d634c0514e37922
Kelly Pratama <46524183+kellytama@users.noreply.github.com>
2019-03-15 11:37:28 -0700

    Create Rank.cs

---
+++ CardLibrary/Rank.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardLibrary
+{
+    public enum Rank
+    {
+        Deuce = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+    }
+}
